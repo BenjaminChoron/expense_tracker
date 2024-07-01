c4cfb8e6f2586ae6848b7f4496c4c6f746e9febd
BenjCH <benjamin.choron@outlook.com>
2024-07-01 12:44:55 +0200

    🚧 Add expense item widget

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/widgets.dart';
+
 class ExpenseItem extends StatelessWidget {
-  const ExpenseItem({super.key});
+  const ExpenseItem({super.key, required this.expense});
+
+  final Expense expense;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(expense.title),
+                Text('${expense.amount.toStringAsFixed(2)} €'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(expense.category.toString()),
+                Text(expense.date.toString()),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
