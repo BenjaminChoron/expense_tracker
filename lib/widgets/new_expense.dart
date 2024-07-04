de953763f3cc50c0ce4032557b4c08b8cff4f993
BenjCH <benjamin.choron@outlook.com>
2024-07-04 18:10:10 +0200

    ♻️ Extract category dropdown widget

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense/category_dropdown.dart';
 import 'package:expense_tracker/widgets/new_expense/new_expense_actions.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -181,33 +182,7 @@ class _NewExpenseState extends State<NewExpense> {
                   Row(
                     children: [
                       Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).colorScheme.secondary),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: DropdownButton(
-                            value: _selectedCategory,
-                            items: Category.values
-                                .map(
-                                  (category) => DropdownMenuItem(
-                                    value: category,
-                                    child: Row(
-                                      children: [
-                                        Icon(categoryIcons[category]),
-                                        const SizedBox(width: 8),
-                                        Text(category.name.toUpperCase()),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                                .toList(),
-                            onChanged: (value) => _selectCategory(value),
-                            underline: const SizedBox(),
-                          ),
-                        ),
+                        child: CategoryDropdown(_selectCategory),
                       ),
                       const SizedBox(width: 16),
                       Expanded(
@@ -268,33 +243,7 @@ class _NewExpenseState extends State<NewExpense> {
                 else
                   Row(
                     children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Theme.of(context).colorScheme.secondary),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: DropdownButton(
-                          value: _selectedCategory,
-                          items: Category.values
-                              .map(
-                                (category) => DropdownMenuItem(
-                                  value: category,
-                                  child: Row(
-                                    children: [
-                                      Icon(categoryIcons[category]),
-                                      const SizedBox(width: 8),
-                                      Text(category.name.toUpperCase()),
-                                    ],
-                                  ),
-                                ),
-                              )
-                              .toList(),
-                          onChanged: (value) => _selectCategory(value),
-                          underline: const SizedBox(),
-                        ),
-                      ),
+                      CategoryDropdown(_selectCategory),
                       const Spacer(),
                       NewExpenseActions(_submitExpenseData),
                     ],
---
+++ lib/widgets/new_expense/category_dropdown.dart
@@ -0,0 +1,39 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class CategoryDropdown extends StatelessWidget {
+  const CategoryDropdown(this._selectCategory, {super.key});
+
+  final void Function(Category) _selectCategory;
+  final Category _selectedCategory = Category.leisure;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Theme.of(context).colorScheme.secondary),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: DropdownButton(
+        value: _selectedCategory,
+        items: Category.values
+            .map(
+              (category) => DropdownMenuItem(
+                value: category,
+                child: Row(
+                  children: [
+                    Icon(categoryIcons[category]),
+                    const SizedBox(width: 8),
+                    Text(category.name.toUpperCase()),
+                  ],
+                ),
+              ),
+            )
+            .toList(),
+        onChanged: (value) => _selectCategory(value!),
+        underline: const SizedBox(),
+      ),
+    );
+  }
+}
